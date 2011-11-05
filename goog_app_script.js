d9cd20c4146619218dd6e5a67fad5c2f1a54a3d6
Jed Wood <jed.wood@gravitytank.com>
2011-11-05 00:15:50 -0500

    first working version

---
+++ goog_app_script.js
@@ -0,0 +1,38 @@
+function main() {
+  
+  var today = Utilities.formatDate(new Date(), "CST", "yyyy-MM-dd"); //make sure you set your timezone
+  var labelCount = GmailApp.getMessagesForThreads(GmailApp.getUserLabelByName("somelabel").getThreads()).length;
+  var unreadCount = GmailApp.getInboxUnreadCount();
+  var starredCount = GmailApp.getMessagesForThreads(GmailApp.getStarredThreads()).length;  
+  var sentTodayCount = GmailApp.getMessagesForThreads(GmailApp.search("from:me after:" + today)).length;
+  var receivedTodayCount = GmailApp.getMessagesForThreads(GmailApp.search("to:me after:" + today)).length;
+  
+  var unamepass ='API_KEY:ignore';
+  var digest = Utilities.base64Encode(unamepass);
+  var digestfull = "Basic "+digest; 
+  var httpheaders = { 
+    "Authorization" : digestfull,
+    "Accept" : "application/json"
+  };
+  
+  var options = {
+    "method" : "post",    
+    "headers" : httpheaders
+  };
+  
+  options.payload = '{"value":' + sentTodayCount + '}'; 
+  var response = UrlFetchApp.fetch("https://push.ducksboard.com/values/ID/", options);
+  
+  options.payload = '{"value":' + receivedTodayCount + '}'; 
+  response = UrlFetchApp.fetch("https://push.ducksboard.com/values/ID/", options);
+  
+  options.payload = '{"value":' + unreadCount + '}'; 
+  response = UrlFetchApp.fetch("https://push.ducksboard.com/values/ID/", options);
+  
+  options.payload = '{"value":' + labelCount + '}'; 
+  response = UrlFetchApp.fetch("https://push.ducksboard.com/values/ID/", options);
+  
+  options.payload = '{"value":' + starredCount + '}'; 
+  response = UrlFetchApp.fetch("https://push.ducksboard.com/values/ID/", options);
+  
+}
