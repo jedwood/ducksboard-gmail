30573a0afeab6e42be3c77724452ed4fa77ed765
Jed Wood <jed.wood@gravitytank.com>
2011-11-05 08:25:01 -0500

    more accurate checking of sender/receiver and date sent by looping through messages in threads.

--- goog_app_script.js
+++ goog_app_script.js
@@ -4,9 +4,28 @@ function main() {
   var labelCount = GmailApp.getMessagesForThreads(GmailApp.getUserLabelByName("somelabel").getThreads()).length;
   var unreadCount = GmailApp.getInboxUnreadCount();
   var starredCount = GmailApp.getMessagesForThreads(GmailApp.getStarredThreads()).length;  
-  var sentTodayCount = GmailApp.getMessagesForThreads(GmailApp.search("from:me after:" + today)).length;
-  var receivedTodayCount = GmailApp.getMessagesForThreads(GmailApp.search("from:(!me) after:" + today)).length;
-  
+  var sentTodayMessages = GmailApp.getMessagesForThreads(GmailApp.search("from:me after:" + today));
+  var receivedTodayMessages = GmailApp.getMessagesForThreads(GmailApp.search("from:(!me) after:" + today));
+
+
+  var receivedTodayCount = 0;
+  var sentTodayCount = 0;
+
+  for (var i = 0 ; i <  receivedTodayMessages.length; i++) {
+    for (var j = 0; j < receivedTodayMessages[i].length; j++) {  
+      var msgDate = Utilities.formatDate(receivedTodayMessages[i][j].getDate(), "CST", "yyyy-MM-dd");
+      if (!(receivedTodayMessages[i][j].getFrom().match('jed@limechile.com')) && (msgDate == today)) receivedTodayCount ++;
+    }
+  }  
+
+
+  for (var i = 0 ; i <  sentTodayMessages.length; i++) {
+    for (var j = 0; j < sentTodayMessages[i].length; j++) {      
+      var msgDate = Utilities.formatDate(sentTodayMessages[i][j].getDate(), "CST", "yyyy-MM-dd");
+      if (sentTodayMessages[i][j].getFrom().match('jed@limechile.com') && msgDate == today) sentTodayCount ++;
+    }
+  }
+      
   var unamepass ='API_KEY:ignore';
   var digest = Utilities.base64Encode(unamepass);
   var digestfull = "Basic "+digest; 
